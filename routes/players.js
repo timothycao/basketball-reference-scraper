e3a8b49f1a8243f435c9a8c5672e1d91769abf6e
Timothy Cao <timothy@Timothys-MacBook-Pro.local>
2022-04-11 16:38:49 -0700

    added category query for other player stats

--- routes/players.js
+++ routes/players.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const baseUrl = 'https://www.basketball-reference.com/players';
 
 router.get('/', async (req, res) => {
-    const { firstName, lastName, playoffs, season, ...stats } = req.query;
+    const { firstName, lastName, category, playoffs, season, ...stats } = req.query;
 
-    const requestedStats = await getRequestedStats(firstName, lastName, baseUrl, 'per_game', playoffs, season, stats);
+    const requestedStats = await getRequestedStats(firstName, lastName, baseUrl, category, playoffs, season, stats);
 
     res.json(requestedStats);
 });
