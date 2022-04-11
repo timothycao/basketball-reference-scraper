f53b40f3e14d35484112908c9a91db8358fc29bc
Timothy Cao <timothy@Timothys-MacBook-Pro.local>
2022-04-10 18:59:36 -0700

    code refactor

--- routes/players.js
+++ routes/players.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 
     const $ = cheerio.load(html);
 
-    const requestedStats = getRequestedStats(stats, $, playoffs, season);
+    const requestedStats = getRequestedStats(stats, $, 'per_game', playoffs, season);
 
     res.json(requestedStats);
 });
@@ -33,24 +33,24 @@ async function getHtml (url) {
     return response.data;
 }
 
-function getTableRow (callback, isPlayoffs, season) {
+function getTableRow (callback, category, isPlayoffs, season) {
     const playoffs = isPlayoffs ? '_playoffs' : '';
 
-    return callback(`#div${playoffs}_per_game table tbody tr[id*=${season}]`);
+    return callback(`#div${playoffs}_${category} table tbody tr[id*=${season}]`);
 }
 
-function getTableRows (callback, isPlayoffs) {
+function getTableRows (callback, category, isPlayoffs) {
     const playoffs = isPlayoffs ? '_playoffs' : '';
 
-    return callback(`#div${playoffs}_per_game table tbody tr`);
+    return callback(`#div${playoffs}_${category} table tbody tr`);
 }
 
 function getTableRowStat (tableRow, stat) {
     return tableRow.find(`[data-stat=${stat}]`).text();
 }
 
-function getSeasonStats (statsObject, callback, isPlayoffs, season, givenTableRow) {
-    const tableRow = givenTableRow || getTableRow(callback, isPlayoffs, season);
+function getSeasonStats (statsObject, callback, category, isPlayoffs, season, givenTableRow) {
+    const tableRow = givenTableRow || getTableRow(callback, category, isPlayoffs, season);
     const seasonStats = { season: getTableRowStat(tableRow, 'season') };
 
     for (let stat in statsObject) {
@@ -60,15 +60,15 @@ function getSeasonStats (statsObject, callback, isPlayoffs, season, givenTableRo
     return seasonStats;
 }
 
-function getCareerStats (statsObject, callback, isPlayoffs) {
-    const tableRows = getTableRows(callback, isPlayoffs);
+function getCareerStats (statsObject, callback, category, isPlayoffs) {
+    const tableRows = getTableRows(callback, category, isPlayoffs);
     const careerStats = [];
 
     tableRows.each(function () {
         const tableRow = callback(this);
         if (!tableRow.attr('id')) return;
 
-        const seasonStats = getSeasonStats(statsObject, undefined, undefined, undefined, tableRow);
+        const seasonStats = getSeasonStats(statsObject, undefined, undefined, undefined, undefined, tableRow);
 
         careerStats.push(seasonStats);
     })
@@ -76,9 +76,9 @@ function getCareerStats (statsObject, callback, isPlayoffs) {
     return careerStats;
 }
 
-function getRequestedStats (statsObject, callback, isPlayoffs, season) {
-    if (season) return getSeasonStats(statsObject, callback, isPlayoffs, season);
-    else return getCareerStats(statsObject, callback, isPlayoffs);
+function getRequestedStats (statsObject, callback, category, isPlayoffs, season) {
+    if (season) return getSeasonStats(statsObject, callback, category, isPlayoffs, season);
+    else return getCareerStats(statsObject, callback, category, isPlayoffs);
 }
 
 module.exports = router;
