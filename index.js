2477ef2b630f855467cafdef7606d78089c5bb9f
timothycao <tcao.pro@gmail.com>
2022-04-18 16:32:02 -0700

    public folder and html mockup

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const players = require('./routes/players');
 const express = require('express');
+const path = require('path');
+
 const app = express()
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.use('/players', players);
 
